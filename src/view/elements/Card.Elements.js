748c59c33b18f4dd064dd69213d2c1ea9dd6750f
qvtec <qvtec3@gmail.com>
2024-05-08 00:51:05 +0900

    ✨ add card_display data

--- src/view/elements/Card.Elements.js
+++ src/view/elements/Card.Elements.js
@@ -5,6 +5,7 @@ export function CardBody({d,card_dim,card_display}) {
       <g transform="translate(${card_dim.text_x}, ${card_dim.text_y})">
         <text clip-path="url(#card_text_clip)">
           <tspan x="${12}" dy="${14}">${card_display[0](d.data)}</tspan>
+          <tspan x="${12}" dy="${30}" font-size="10">${card_display[1](d.data)}</tspan>
         </text>
         <rect width="${card_dim.w-card_dim.text_x-10}" height="${card_dim.h-20}" style="mask: url(#fade)" class="text-overflow-mask" />
       </g>
